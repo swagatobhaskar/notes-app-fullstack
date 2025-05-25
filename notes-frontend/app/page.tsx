3365e158b6e007fad3e102ae7f02cd2ba8428566
swagatobhaskar <bhaskarswagato@gmail.com>
2025-05-25 23:24:35 +0530

    WIP: background image property

--- notes-frontend/app/page.tsx
+++ notes-frontend/app/page.tsx
@@ -1,32 +1,25 @@
 import Image from "next/image";
-import img from '../public/hugo-rocha-unsplash.jpg';
 
 export default async function Home() {
-
-  const imageStyle = {
-    borderRadius: '5%',
-    border: '1px solid #fff',
-    width: '1000px',
-    height: 'auto',
-  }
 
   return (
     <div className="flex flex-col min-h-screen">
       <header className="bg-gray-800 text-white p-4 text-center">
-
+        <p>Example App</p>
       </header>
       <main className="flex-1">
-        <div className="mx-auto w-2/3 shadow shadow-gray-400 p-5">
+        <div className="mx-auto w-2/3">
           <Image 
-            src={img}
+            src='/hugo-rocha-unsplash.jpg'
             alt="brainstorm-notes"
-            placeholder="blur"
+            placeholder="empty"
             quality={100}
             fill
             sizes="100vw"
             style={{
               objectFit: 'cover',
             }}
+            className="opacity-60 blur-xs"
           />
         </div>
       </main>
