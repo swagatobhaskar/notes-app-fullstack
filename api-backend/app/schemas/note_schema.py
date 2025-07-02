3e1a4f78a36cb4e295bf8deea2213db7a7fe0e59
swagatobhaskar <bhaskarswagato@gmail.com>
2025-07-03 01:42:26 +0530

    Edit: Modified Schema definitions

--- api-backend/app/schemas/note_schema.py
+++ api-backend/app/schemas/note_schema.py
@@ -3,26 +3,29 @@ from pydantic import BaseModel, Field, field_validator, model_validator
 from datetime import datetime
 import re
 
-# from app.models import Tag
 from .user_schema import UserOut
 from .tag_schema import TagOut
+from .folder_schema import FolderOut
+
+"""
+If you want an optional list: List[int] | None = None
+If you always want a list: List[int] = [] or Field(default_factory=list)
+When you POST new notes, the client should not send id, user_id, created_at,
+    updated_at. These should come from the DB.
+"""
+
+# Base should never have DB-generated fields or nested resolved objects.
+# It’s just the common request body shape
 class NoteBase(BaseModel):
-    id: int | None = None   # auto-populated
-    user_id: int | None = None  # auto-populated
-    owner: UserOut | None = None    # auto-populated
     title: str
     content: str
-    created_at: datetime | None = None  # auto-populated
-    updated_at: datetime | None = None  # auto-populated
-    tag_ids: List[int] = []
 
-    class Config:
-        arbitrary_types_allowed=True
-        from_attributes = True # Needed to read SQLAlchemy objects
-
+# NoteCreate: only fields you expect from the user/client
 class NoteCreate(NoteBase):
     title: str = Field(..., min_length=10, max_length=50)
     content: str = Field(..., min_length=10)
+    tag_ids: List[int] | None = None
+    folder_id: Optional[int] = None
     
     @field_validator('title')
     @classmethod
@@ -38,15 +41,25 @@ class NoteCreate(NoteBase):
             raise ValueError("Title can only contain letters, numbers, spaces, hyphens, and underscores.")
         return v
 
-class NoteOut(NoteBase):
-    tags: List[TagOut]
+
+# Output: includes DB-populated fields + nested relations + from notebase.
+class NoteOut(NoteBase):    
+    id: int   # auto-populated
+    user_id: int  # auto-populated
+    owner: UserOut | None = None    # auto-populated
+    created_at: datetime | None = None  # auto-populated
+    updated_at: datetime | None = None  # auto-populated
+    tags: List[TagOut] | None = None
+    folder: FolderOut | None = None
     class Config:
-        from_attributes = True # Needed to read SQLAlchemy objects
+        from_attributes = True
 
 # for use with PATCH method
 class NoteUpdate(BaseModel):
     title: Optional[str] = None
     content: Optional[str] = None
+    tag_ids: Optional[List[int]] = None
+    folder_id: Optional[int] = None
 
     @field_validator('title')
     @classmethod
