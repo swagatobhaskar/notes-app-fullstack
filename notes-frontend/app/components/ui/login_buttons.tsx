0489735e53babf471ecbe40b6f8ea28797ce56e1
swagatobhaskar <bhaskarswagato@gmail.com>
2025-07-05 00:29:17 +0530

    WIP: folders page

--- notes-frontend/app/components/ui/login_buttons.tsx
+++ notes-frontend/app/components/ui/login_buttons.tsx
@@ -47,12 +47,12 @@ export default function LoginButtons({email}: {email: string | null }) {
     return (
         <div className="flex flex-row space-x-1.5">
             <Link
-                href={"/user/login"}
+                href={"/login"}
                 className="bg-gray-300 hover:bg-white hover:cursor-pointer text-black px-2 py-1 rounded-sm"
                 >
                     Login
             </Link>
-            <Link href={"/user/register"}
+            <Link href={"/register"}
                 className="bg-gray-300 hover:bg-white hover:cursor-pointer text-black px-2 py-1 rounded-sm"
                 >
                     Sign Up
