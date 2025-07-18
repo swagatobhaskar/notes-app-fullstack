bbac9785924cfe3acef931b6e10b1d863bf2b4ef
swagatobhaskar <bhaskarswagato@gmail.com>
2025-07-19 02:29:24 +0530

    Add: standalone mode for Docker deployment

--- notes-frontend/next.config.ts
+++ notes-frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",   // Added to run production app in Docker
 };
 
 export default nextConfig;
